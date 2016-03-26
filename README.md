e8c22e72021bddbe5d02bba9c3b0ba4d0920c0ff
Steve Dignam <steve@dignam.xyz>
2016-03-26 19:29:11 -0400

    removed jsdom, minor changes to gen-camera-data, updated cameras.json 🚍

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ You can also specify a certain number of feeds to load via a hash at the end of
 the URL.
 
 [`http://nyc.narp.xyz/#40`](http://nyc.narp.xyz/#40)
+
+## Note
+
+There around 425 active cameras and each camera feed provides roughly 20 KB images.
+
+`425 * 20 KB = 8.5 MB`
+
+So if you are expecting all the images to be loaded and updated every second,
+make sure you have the bandwidth.
--- gen-camera-data.js
+++ gen-camera-data.js
@@ -1,63 +1,64 @@
-#!/usr/bin/env node
+(function() {
+    /* global document console */
+    'use strict';
 
-/* global require console */
+    // Note: this is intended to be run via the console at `http://dotsignals.org/multiview2.php`
+    // to generate camera data
 
-var jsdom = require("jsdom");
+    var manhattan = Array.from(document.querySelectorAll("tbody > tr > td > table#tableCam > tbody > tr#repCam__ctl0_trCam"));
+    var bronx = Array.from(document.querySelectorAll("tbody > tr > td > table#tableCam1 > tbody > tr#repCam__ctl0_trCam"));
+    var brooklyn = Array.from(document.querySelectorAll("tbody > tr > td > table#tableCam2 > tbody > tr#repCam__ctl0_trCam"));
+    var queens = Array.from(document.querySelectorAll("tbody > tr > td > table#tableCam3 > tbody > tr#repCam__ctl0_trCam"));
+    var statenIsland = Array.from(document.querySelectorAll("tbody > tr > td > table#tableCam4 > tbody > tr#repCam__ctl0_trCam"));
 
-var URL = "http://dotsignals.org/multiview2.php";
 
-jsdom.env(
-    URL,
-    function(err, window) {
-        'use strict';
-        var document = window.document;
+    var query = function(elements, area) {
+        var result = {};
+        elements.forEach(function(x) {
+            var cameraRaw = x.querySelectorAll("tr > td > input")[0];
+            if (!cameraRaw) {
+                // this will return when the camera is inactive
+                return;
+            }
 
-        var manhattan = Array.from(document.querySelectorAll("tbody > tr > td > table#tableCam > tbody > tr#repCam__ctl0_trCam"));
-        var bronx = Array.from(document.querySelectorAll("tbody > tr > td > table#tableCam1 > tbody > tr#repCam__ctl0_trCam"));
-        var brooklyn = Array.from(document.querySelectorAll("tbody > tr > td > table#tableCam2 > tbody > tr#repCam__ctl0_trCam"));
-        var queens = Array.from(document.querySelectorAll("tbody > tr > td > table#tableCam3 > tbody > tr#repCam__ctl0_trCam"));
-        var statenIsland = Array.from(document.querySelectorAll("tbody > tr > td > table#tableCam4 > tbody > tr#repCam__ctl0_trCam"));
+            // get local e.g. queens
+            var locale = x.querySelectorAll("tr > td > span")[0].innerText;
+            if (!locale) {
+                locale = "unknown";
+            }
 
-        var query = function(elements, area) {
-            var result = {};
-            elements.forEach(function(x) {
-                var cameraRaw = x.querySelectorAll("tr > td > input")[0];
-                if (!cameraRaw) {
-                    // this will return when the camera is inactive
-                    return;
-                }
-                var camera = cameraRaw.value;
+            var camera = cameraRaw.value;
+            result[camera] = {
+                "locale": locale,
+                "area": area
+            };
+        });
+        return result;
+    };
 
-                var locale = x.querySelectorAll("tr > td > span.OTopTitle")[0].innerText;
 
-                result[camera] = {
-                    locale: locale,
-                    area: area
-                };
-            });
-            return result;
-        };
-
-        var join = function(o1, o2) {
-            for (var key in o2) {
-                if (o2.hasOwnProperty(key)) {
-                    o1[key] = o2[key];
+    var join = function() {
+        var obj = {};
+        for (var i = 0; i < arguments.length; i++) {
+            var arg = arguments[i];
+            for (var key in arg) {
+                if (arg.hasOwnProperty(key)) {
+                    obj[key] = arg[key];
                 }
             }
-            return o1;
-        };
+        }
+        return obj;
+    };
 
-        var r1 = query(manhattan, "manhattan");
-        var r2 = query(bronx, "bronx");
-        var r3 = query(brooklyn, "brooklyn");
-        var r4 = query(queens, "queens");
-        var r5 = query(statenIsland, "statenIsland");
 
-        var x = join(r1, r2);
-        x = join(x, r3);
-        x = join(x, r4);
-        x = join(x, r5);
+    var r1 = query(manhattan, "manhattan");
+    var r2 = query(bronx, "bronx");
+    var r3 = query(brooklyn, "brooklyn");
+    var r4 = query(queens, "queens");
+    var r5 = query(statenIsland, "statenIsland");
 
-        console.log(JSON.stringify(x));
-    }
-);
+    var x = join(r1, r2, r3, r4, r5);
+
+
+    console.log(JSON.stringify(x));
+})();
